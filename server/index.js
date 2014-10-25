5843a5fab9418cff851382c1e708472d80c7f276
Shoubhik <sbose78@gmail.com>
2014-10-25 14:23:47 -0400

    Added socket clients

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.get('/broadcast' , function( req, res){
  	var lat = req.query.lat ;
 	var lon = req.query.lon ;
         io.emit('maps',lat+","+lon);
-	res.send('Send broadcast message successfully ');
+        client.write(lat+":"+lon);
+          
+	res.send('Sent broadcast message successfully ');
      
 });
 
@@ -33,6 +35,73 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3007, function(){
+http.listen(3008, function(){
   console.log('listening on *:3000');
+});
+
+
+/* Socket implementation */
+
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6970;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+// Whenever a new location data is recieved , write to the socket stream
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA  ( recieved by server ) ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write(data);
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+
+
+
+/*** Socket client for relaying the location info to the localhost socket server, which in turn would send it to the device socket client ***/
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function() {
+
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    client.write('I am Chuck Norris!');
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+    
+    console.log('DATA: ' + data);
+    // Close the client socket completely
+
+    if ( data == "-1:-1" )
+    {
+    client.destroy();
+    }
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
 });
